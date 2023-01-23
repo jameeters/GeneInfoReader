101846f652113d60076a8c2c3ded08b61c3bac73
James McFeeters <jameeters@gmail.com>
2023-01-23 15:54:12 -0600

    Improve CLI usability

--- src/main/java/org/pankratzlab/Main.java
+++ src/main/java/org/pankratzlab/Main.java
@@ -1,18 +1,43 @@
 package org.pankratzlab;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
   public static void main(String[] args) {
-    String gffFile = args[0];
+    // @formatter:off
+    String usage = "\n" + "GeneInfoReaderUsage: \n"
+                   + "dir=./  specify working directory (optional)\n"
+                   + "inputFile=... specify gff3 input file (required)\n"
+                   + "qc=true whether to output qc files (optional)\n" + "\n";
+    // @formatter:on
+
+    Path inputFile = null;
     boolean qc = true;
-    if (args.length == 2) {
-      if (args[1].equals("-noqc")) {
-        qc = false;
+    Path dir = Paths.get("./");
+
+    for (String arg : args) {
+      if (arg.startsWith("dir=")) {
+        dir = Paths.get(arg.split("=")[1]);
+      } else if (arg.startsWith("inputFile=")) {
+        Path p = Paths.get(arg.split("=")[1]);
+        inputFile = dir.resolve(p);
+      } else if (arg.startsWith("qc=")) {
+        qc = Boolean.parseBoolean(arg.split("=")[1]);
+      } else {
+        System.out.println(usage);
+        System.exit(1);
       }
     }
 
-    Aggregator aggregator = new Aggregator(gffFile);
+    if (inputFile == null) {
+      System.err.println("No input file provided!");
+      System.out.println(usage);
+      System.exit(1);
+    }
+
+    Aggregator aggregator = new Aggregator(dir, inputFile);
 
     aggregator.findGenesAndExons();
     aggregator.computeXRefMap();
